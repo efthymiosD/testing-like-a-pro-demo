278ddd403d6bcfae262592bec8933f120d8d034f
Efthymis Dks <efthymis@Efthymiss-MacBook-Pro.local>
2021-01-10 21:02:44 +0100

    Initial commit :)

---
+++ src/main/java/com/dks/testinglikeaprodemo/TestingLikeAProDemoApplication.java
@@ -0,0 +1,13 @@
+package com.dks.testinglikeaprodemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingLikeAProDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingLikeAProDemoApplication.class, args);
+	}
+
+}
